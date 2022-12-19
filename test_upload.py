274f0f0f065334904a6512e3e3a873914bfc7b41
Evan Lewis <ehlewis@me.com>
2022-12-19 13:59:36 -0700

    updates requirements

--- test_upload.py
+++ test_upload.py
@@ -1,9 +1,9 @@
 import requests
 import time
 
-url = 'http://127.0.0.1:8000/scan-file'
+url = 'http://127.0.0.1:5000/scan-file'
 for x in range(10):
-    with open(r"Docker.exe", 'rb') as file:
+    with open(r"testfiles\\Docker.exe", 'rb') as file:
         file = {'file': file}
         resp = requests.post(url=url, files=file) 
         print(resp.json())
